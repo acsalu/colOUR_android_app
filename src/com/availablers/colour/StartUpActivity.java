27a40e9d7729e782be26da0bfad6fe7c6ecfe21a
Acsa Lu <acsa0210@gmail.com>
2012-11-25 20:24:44 +0800

    use Facebook login

--- src/com/availablers/colour/StartUpActivity.java
+++ src/com/availablers/colour/StartUpActivity.java
@@ -1,10 +1,17 @@
 package com.availablers.colour;
 
-import com.parse.Parse;
-import com.parse.ParseFacebookUtils;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
 
 public class StartUpActivity extends Activity {
 
@@ -13,10 +20,14 @@ public class StartUpActivity extends Activity {
 	private static final String PARSE_CLIENT_KEY = "RS2i8WSFWnIgrN3NNFIApvymSEIeddGzyjzpAoMJ";
 	private static final String FACEBOOK_APP_ID = "509468125744251";
 	
+	// UI Components
+	private Button buttonLogin;
+	
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_startup);
 		initParse();
 		findViews();
 		setListeners();
@@ -29,11 +40,33 @@ public class StartUpActivity extends Activity {
 	
 	
 	private void findViews() {
-		
+		this.buttonLogin = (Button) findViewById(R.id.button_login);
 	}
 	
 	private void setListeners() {
-		
+		this.buttonLogin.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				ParseFacebookUtils.logIn(StartUpActivity.this, new LogInCallback() {
+					  @Override
+					  public void done(ParseUser user, ParseException err) {
+					    if (user == null) {
+					      Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
+					      return;
+					    } else if (user.isNew()) {
+					      Log.d("MyApp", "User signed up and logged in through Facebook!");
+					    } else {
+					      Log.d("MyApp", "User logged in through Facebook!");
+					    } 
+					    
+					    Intent intent = new Intent(getBaseContext(), MainActivity.class);
+					    startActivity(intent);
+					    finish();
+					    
+					  }
+				});
+			}
+		});
 	}
 	
 }
